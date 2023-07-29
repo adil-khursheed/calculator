641c5c4aebc5389e8d2b7f523d0adeb5e9bfdd08
adil-khursheed <adilkhursheed60@gmail.com>
2023-07-29 16:19:54 +0530

    theme toggle

---
+++ script.js
@@ -0,0 +1,27 @@
+const colorThemes = document.querySelectorAll("[name='theme']");
+
+const storeTheme = function (theme) {
+  localStorage.setItem("theme", theme);
+};
+
+const setTheme = function () {
+  const activeTheme = localStorage.getItem("theme");
+
+  colorThemes.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    }
+  });
+
+  document.documentElement.className = activeTheme;
+};
+
+colorThemes.forEach((themeOption) => {
+  themeOption.addEventListener("click", () => {
+    storeTheme(themeOption.id);
+
+    document.documentElement.className = themeOption.id;
+  });
+});
+
+document.onload = setTheme();
