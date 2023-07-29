236f084a7da08ef4a95ae8f0d633d1dbf20f1d82
adil-khursheed <adilkhursheed60@gmail.com>
2023-07-29 18:11:11 +0530

    calculation func

--- script.js
+++ script.js
@@ -25,3 +25,41 @@ colorThemes.forEach((themeOption) => {
 });
 
 document.onload = setTheme();
+// ====== Theme functions end ====== //
+
+// ====== Calculator functions ====== //
+let currentInput = "";
+let result = "";
+
+function updateDisplay() {
+  const display = document.getElementById("display");
+
+  display.value = currentInput;
+}
+
+function appendToDisplay(value) {
+  currentInput += value;
+  updateDisplay();
+}
+
+function resetDisplay() {
+  currentInput = "";
+  result = "";
+  updateDisplay();
+}
+
+function correctInput() {
+  currentInput = currentInput.slice(0, -1);
+  updateDisplay();
+}
+
+function calculateResult() {
+  try {
+    result = eval(currentInput);
+    currentInput = result.toString();
+    updateDisplay();
+  } catch (error) {
+    currentInput = "Error";
+    updateDisplay();
+  }
+}
